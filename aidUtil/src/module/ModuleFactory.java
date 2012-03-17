2693cb2eb3e170ac9b5a172101358f86ba314747
dozedoff <dozedoffagain@gmail.com>
2012-03-17 14:09:17 +0100

    finished ModuleFactory

--- aidUtil/src/module/ModuleFactory.java
+++ aidUtil/src/module/ModuleFactory.java
@@ -1,5 +1,7 @@
 package module;
 
 public class ModuleFactory {
-
+	public static MaintenanceModule createModule(String moduleName) throws Exception{
+		return (MaintenanceModule) Class.forName(moduleName).newInstance();
+	}
 }
