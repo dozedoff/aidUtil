bfa6b149aa6d407856b56e286b5b44ebe0bcece7
dozedoff <dozedoffagain@gmail.com>
2012-04-30 10:57:33 +0200

    added module for hashing files

---
+++ aidUtil/src/module/ModuleHashFiles.java
@@ -0,0 +1,52 @@
+/*  Copyright (C) 2012  Nicholas Wright
+	
+	part of 'AidUtil', a collection of maintenance tools for 'Aid'.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package module;
+
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextArea;
+
+public class ModuleHashFiles extends MaintenanceModule implements ActionListener {
+	JTextArea logArea;
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void optionPanel(Container container) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Cancel() {
+		// TODO Auto-generated method stub
+		
+	}
+}
