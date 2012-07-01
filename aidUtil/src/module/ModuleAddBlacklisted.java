3e97899d65a6f217d7ff01d41630683ff57ea96d
dozedoff <dozedoffagain@gmail.com>
2012-07-01 16:08:20 +0200

    Created file

---
+++ aidUtil/src/module/ModuleAddBlacklisted.java
@@ -0,0 +1,42 @@
+/*  Copyright (C) 2012  Nicholas Wright
+	
+	part of 'AidUtil', a collection of maintenance tools for 'Aid'.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package module;
+
+import java.awt.Container;
+
+public class ModuleAddBlacklisted extends MaintenanceModule {
+
+	@Override
+	public void optionPanel(Container container) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Cancel() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
