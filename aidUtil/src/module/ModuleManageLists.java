297406cefca129af90f02673ae13f4a15bbd87cc
dozedoff <dozedoffagain@gmail.com>
2012-07-11 19:46:32 +0200

    adding a file to DNW or blacklist will remove it from the index

--- aidUtil/src/module/ModuleManageLists.java
+++ aidUtil/src/module/ModuleManageLists.java
@@ -134,9 +134,11 @@ public class ModuleManageLists extends MaintenanceModule {
 				if(listDnw.isSelected()){
 					sql.update(hash, AidTables.Dnw);
 					sql.delete(AidTables.Block, hash);
+					sql.deleteIndexByPath(file.toString());
 				}else if(listBlacklist.isSelected()){
 					sql.update(hash, AidTables.Block);
 					sql.delete(AidTables.Dnw, hash);
+					sql.deleteIndexByPath(file.toString());
 				}else if(listUnDnw.isSelected()){
 					sql.delete(AidTables.Dnw, hash);
 				}else if(listUnBlacklist.isSelected()){
