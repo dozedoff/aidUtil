c78bdbed37cfa2f5beb70fbff6cbfce02c63abd9
dozedoff <dozedoffagain@gmail.com>
2012-08-03 23:11:39 +0200

    refactored Core.loadModules()

--- aidUtil/src/module/ModuleFactory.java
+++ aidUtil/src/module/ModuleFactory.java
@@ -18,7 +18,7 @@
 package module;
 
 public class ModuleFactory {
-	public static MaintenanceModule createModule(String moduleName) throws Exception{
+	public static MaintenanceModule createInstance(String moduleName) throws Exception{
 		return (MaintenanceModule) Class.forName(moduleName).newInstance();
 	}
 }
