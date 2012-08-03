d81efb248293a29fa1ee2c8f7b1d31677252ec94
dozedoff <dozedoffagain@gmail.com>
2012-08-03 15:38:25 +0200

    created thread to add hashed files to the database

---
+++ aidUtil/src/module/DatabaseWorker.java
@@ -0,0 +1,68 @@
+/*  Copyright (C) 2012  Nicholas Wright
+	
+	part of 'AidUtil', a collection of maintenance tools for 'Aid'.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package module;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+import file.FileInfo;
+
+public class DatabaseWorker extends Thread{
+	private final DatabaseHandler dbHandler;
+	private final LinkedBlockingQueue<FileInfo> inputQueue;
+	private final OperationMode mode;
+	
+	public static enum OperationMode {AddToIndex, AddToDNW};
+	
+	public DatabaseWorker(DatabaseHandler dbHandler, LinkedBlockingQueue<FileInfo> inputQueue, OperationMode mode) {
+		super("Database worker");
+		this.dbHandler = dbHandler;
+		this.inputQueue = inputQueue;
+		this.mode = mode;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			doWork();
+		} catch (InterruptedException e) {
+			interrupt();
+		}
+	}
+	
+	private void doWork() throws InterruptedException {
+		while(! isInterrupted()){
+			FileInfo currentFile = inputQueue.take();
+			addToDatabse(currentFile);
+		}
+	}
+	
+	private void addToDatabse(FileInfo info){
+		switch (mode) {
+		case AddToIndex:
+			dbHandler.addIndex(info);
+			break;
+			
+		case AddToDNW:
+			dbHandler.addDnw(info);
+			break;
+			
+		default:
+			break;
+		}
+	}
+}
