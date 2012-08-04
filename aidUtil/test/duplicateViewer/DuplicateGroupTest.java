e54c25c6d41e8613e1f3ab012ac7a50ae07f945f
dozedoff <dozedoffagain@gmail.com>
2012-08-04 12:45:23 +0200

    added test cases

---
+++ aidUtil/test/duplicateViewer/DuplicateGroupTest.java
@@ -0,0 +1,66 @@
+/*  Copyright (C) 2012  Nicholas Wright
+	
+	part of 'AidUtil', a collection of maintenance tools for 'Aid'.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package duplicateViewer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DuplicateGroupTest {
+
+	@Test
+	public void testAddEntry() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveEntry() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEntries() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGroupId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAreAllSelected() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasOnlyOneEntry() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+}
